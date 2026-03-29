8883338961f9acc6d31fa40742722281c2921cd4
rafflores17-prog <rafflores17@gmail.com>
2026-03-28 23:48:55 -0300

    Update fetch URL and error handling in gerarLinkCurto

--- painel.js
+++ painel.js
@@ -1,7 +1,7 @@
 function gerarLinkCurto(urlReal) {
     const id = Math.random().toString(36).substring(2,8).toUpperCase();
 
-    fetch('/api/links', {
+    fetch('https://apkbugadovip.vercel.app/api/links', { // coloque a URL do site principal
         method: 'POST',
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify({ id, url: urlReal })
@@ -9,8 +9,10 @@ function gerarLinkCurto(urlReal) {
     .then(res => res.json())
     .then(data => {
         if(data.success){
-            console.log('Link gerado:', id);
             document.getElementById('resultado').innerText = `${window.location.origin}/lk/${id}`;
+        } else {
+            document.getElementById('resultado').innerText = 'Erro ao gerar link';
         }
-    });
+    })
+    .catch(e => document.getElementById('resultado').innerText = 'Erro de conexão');
 }
